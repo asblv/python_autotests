c5bcf8a683e95cc527e0018c5c3f8b82cba2dbc1
asblv <exbl4de@gmail.com>
2024-11-14 20:35:32 +0500

    Update main.py

--- Autotests/Python/main.py
+++ Autotests/Python/main.py
@@ -7,9 +7,7 @@ body_create = {
     "photo_id": -1
 }
 
-
 response_create = requests.post(url= f'{URL}/pokemons', headers = HEADER, json = body_create)
-assert response_create.status_code == 201
 print(response_create.text)
 
 pokemon_id = response_create.json()['id']
@@ -26,12 +24,10 @@ body_pokemon_id = {
 }
 response_rename = requests.patch(url= f'{URL}/pokemons', headers = HEADER, json = body_rename)
 print(response_rename.text)
-assert response_rename.status_code == 200
 
 response_pokeball = requests.post(url= f'{URL}/trainers/add_pokeball', headers = HEADER, json = body_pokemon_id)
 print(response_pokeball.text)
-assert response_pokeball.status_code == 200
 
 response_knockout = requests.post(url= f'{URL}/pokemons/knockout', headers = HEADER, json = body_pokemon_id)
 print(response_knockout.text)
-assert response_knockout.status_code == 200
+
